91e99f88f99d227784087ba386f76a888407f5db
arcadag <piknik0711@yandex.ru>
2023-03-21 22:06:34 +0300

    Example of weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [temp, setTemp] = useState('');
+  const [desc, setDesc] = useState('');
+  const [icon, setIcon] = useState('');
+  const [isReady, setReady] = useState(false);
+
+  React.useEffect(() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&appid=12c14c8d49d63968b9f9fa5f4e8ff7af')
+    .then(result => result.json())
+    .then(jsonresult => {
+      setTemp(jsonresult.main.temp);
+      setDesc(jsonresult.weather[0].description);
+      setIcon(jsonresult.weather[0].icon);
+      setReady(true);
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  if(isReady) {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Tempreture: {temp}</p>
+      <p>Description: {desc}</p>
+      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="Weather icon"/>
     </div>
   );
+  } else {
+    return <div>Loading...</div>
+  }
+  
 }
 
 export default App;
